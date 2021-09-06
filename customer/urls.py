c6f4b93a6b1c824382d88f47bf20489c2cbdeccc
sina <sinaesmaili216@gmail.com>
2021-09-06 16:59:41 +0430

    login and register pages provided and edit profile view created

--- customer/urls.py
+++ customer/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import register_customer, login_customer, profile, logout_customer
+from .views import register_customer, login_customer, profile, logout_customer, edit_profile
 
 app_name = 'customer'
 urlpatterns = [
     path('register/', register_customer, name='register'),
     path('login/', login_customer, name='login'),
     path('logout/', logout_customer, name='logout'),
-    path('profile/', profile, name='profile')
+    path('profile/', profile, name='profile'),
+    path('edit_profile', edit_profile, name='edit_profile')
 ]
--- customer/views.py
+++ customer/views.py
@@ -8,47 +8,37 @@ from .models import Customer
 
 def register_customer(request):
     if request.method == 'POST':
-        form = RegisterCustomer(request.POST)
-        if form.is_valid():
-            first_name = form.cleaned_data.get('first_name')
-            last_name = form.cleaned_data.get('last_name')
-            email = form.cleaned_data.get('email')
-            phone = form.cleaned_data.get('phone')
-            password = form.cleaned_data.get('password')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        password = request.POST.get('password')
 
-            create_user = User.objects.create_user(email=email, password=password)
-            create_user.is_customer = True
-            create_user.save()
+        create_user = User.objects.create_user(email=email, password=password)
+        create_user.is_customer = True
+        create_user.save()
 
-            create_customer = Customer.objects.create(user=create_user, first_name=first_name, last_name=last_name, phone=phone)
-            create_customer.save()
-            login(request, create_user)
+        create_customer = Customer.objects.create(user=create_user, first_name=first_name, last_name=last_name, phone=phone)
+        create_customer.save()
+        login(request, create_user)
 
-            return redirect('product:index')
+        return redirect('product:index')
 
-    else:
-        form = RegisterCustomer()
-
-    return render(request, 'customer/register_customer.html', context={'form': form})
+    return render(request, 'customer/register_customer.html', context={})
 
 
 def login_customer(request):
     if request.method == 'POST':
-        form = LoginCustomer(request.POST)
-        if form.is_valid():
-            email = form.cleaned_data.get('email')
-            password = form.cleaned_data.get('password')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(email=email, password=password)
+        if user:
+            login(request, user)
+            return redirect('product:index')
+        else:
+            return HttpResponse('email or password is incorrect')
 
-            user = authenticate(email=email, password=password)
-            if user:
-                login(request, user)
-                return redirect('product:index')
-            else:
-                return HttpResponse('email or password is incorrect')
-    else:
-        form = LoginCustomer()
-
-    return render(request, 'customer/login_customer.html', context={'form': form})
+    return render(request, 'customer/login_customer.html', context={})
 
 
 def logout_customer(request):
@@ -60,3 +50,24 @@ def profile(request):
     user = request.user
     customer = Customer.objects.get(user=user)
     return render(request, 'customer/profile.html', context={'customer': customer})
+
+
+def edit_profile(request):
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        phone = request.POST.get('phone')
+
+        user = request.user
+        customer = Customer.objects.get(user=user)
+
+        customer.first_name = first_name
+        customer.last_name = last_name
+        customer.phone = phone
+        customer.save()
+
+        return redirect('customer:profile')
+
+    return render(request, 'customer/edit_profile.html')
+
+
