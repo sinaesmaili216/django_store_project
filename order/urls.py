fd5afa04e8189e91510fd39e563176904dbfc02d
sina <sinaesmaili216@gmail.com>
2021-09-08 02:09:01 +0430

    step 1 created for ordering

--- order/urls.py
+++ order/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from order.views import cart_items
+from order.views import cart_items, order
 
 app_name = 'order'
 urlpatterns = [
-    path('cart_items/', cart_items, name='cart-items')
+    path('cart_items/', cart_items, name='cart-items'),
+    path('order/<pk>', order, name='order')
 ]
--- order/views.py
+++ order/views.py
@@ -7,3 +7,9 @@ def cart_items(request):
     products = request.session['products']['products']
 
     return render(request, 'order/show_cart_items.html', context={'products': products})
+
+
+def order(request, pk):
+    product = Product.objects.get(id=pk)
+    return render(request, 'order/order.html', context={'product': product})
+
