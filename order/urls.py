95553f3a0c1018de36682f62b68e1bd51609dff2
sina <sinaesmaili216@gmail.com>
2021-09-08 01:10:56 +0430

    shopping cart created

--- order/urls.py
+++ order/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
+from order.views import cart_items
 
 app_name = 'order'
 urlpatterns = [
-    #path('cart/<pk>', cart, name='cart')
+    path('cart_items/', cart_items, name='cart-items')
 ]
--- order/views.py
+++ order/views.py
@@ -1,10 +1,9 @@
-# from django.http import HttpResponse
-# from django.shortcuts import render, redirect
-# from product.models import Product
-#
-#
-# def cart(request, pk):
-#     request.session['products'] = Product.objects.get(id=pk)
-#     print(request.session['products'])
-#
-#     return redirect('product:index')
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from product.models import Product
+
+
+def cart_items(request):
+    products = request.session['products']['products']
+
+    return render(request, 'order/show_cart_items.html', context={'products': products})
